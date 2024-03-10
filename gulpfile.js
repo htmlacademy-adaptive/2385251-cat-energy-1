01edb825175ede2a5ab203c8be8882403a60e941
Igor <ilidaN853@gmail.com>
2024-03-10 21:32:32 +0300

    Отшлифовает с Pixel Perfect

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ import browser from 'browser-sync';
 import htmlmin from 'gulp-htmlmin';
 import squoosh from 'gulp-libsquoosh';
 import terser from 'gulp-terser';
-import svgstore from 'gulp-svgstore';
+import {stacksvg} from 'gulp-stacksvg';
 import svgo from 'gulp-svgmin';
 import {deleteAsync} from 'del';
 
@@ -65,7 +65,7 @@ const createWebp = () => {
 
 //Svg
 const svg = () => {
-  return gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+  return gulp.src(['source/img/**/*.svg', '!source/img/icons/*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest('build/img'))
 }
@@ -73,10 +73,9 @@ const svg = () => {
 const sprite = () => {
   return gulp.src('source/img/icons/*.svg')
     .pipe(svgo())
-    .pipe(svgstore( {
-      inlineSvg: true
+    .pipe(stacksvg( {
+      output: 'sprite'
     }))
-    .pipe(rename('sprite.svg'))
     .pipe(gulp.dest('build/img'))
 }
 
